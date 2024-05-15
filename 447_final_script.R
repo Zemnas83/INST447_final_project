4d3ea5f33afe07bc6663ee20aeef8d185e02a2ca
zmat83 <zayanmatin@gmail.com>
2024-05-15 18:24:02 -0400

    box plot added for ivy league  vs non ivy league starting median salaries

--- 447_final_script.R
+++ 447_final_script.R
@@ -17,11 +17,32 @@ getmode <- function(v) {
 
 # Filter data for "Ivy" school type and calculate the mean, median, and mode
 ivy_stats <- collegedata %>%
-  filter(School.Type == "Ivy") %>%
+  filter(School.Type == "Ivy League") %>%
   summarise(
     Mean = mean(Starting.Median.Salary, na.rm = TRUE),
     Median = median(Starting.Median.Salary, na.rm = TRUE),
     Mode = getmode(Starting.Median.Salary)
   )
-print(ivy_stats)
 
+non_ivy_stats <- collegedata %>%
+  filter(School.Type != "Ivy League") %>%
+  summarise(
+    Mean = mean(Starting.Median.Salary, na.rm = TRUE),
+    Median = median(Starting.Median.Salary, na.rm = TRUE),
+    Mode = getmode(Starting.Median.Salary)
+  )
+
+print(ivy_stats)
+print(non_ivy_stats)
+
+# box plot of ivy league data vs non ivy league data
+collegedata$Category <- if_else(collegedata$School.Type == "Ivy League", "Ivy League", "Non-Ivy League")
+
+ggplot(collegedata, aes(x = Category, y = Starting.Median.Salary, fill = Category)) +
+  geom_boxplot() +
+  labs(title = "Box Plot of Starting Median Salaries: Ivy League vs. Non-Ivy League",
+       x = "Category",
+       y = "Starting Median Salary",
+       fill = "Category") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))  # Adjust text angle for better legibility
